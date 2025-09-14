a58eeea190a867fe0aaf5b215f25d46e35587541
Azmir Khan <azmir.azamkhan@gmail.com>
2025-09-14 23:26:19 +0600

    Intial project setup.

---
+++ android/app/src/main/java/com/assignment/assignment_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.assignment.assignment_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp( new MyApp() );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
